ecd47195976b47c0d83edf2f7656112a60667298
tabco7556282 <tabco7556282@gmail.com>
2025-09-01 16:37:06 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "shiire-cache-v21.0";
+const CACHE = "shiire-cache-v21.1";
 const ASSETS = ["./","./index.html","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
 
 self.addEventListener("install", (e) => {
